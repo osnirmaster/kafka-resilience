8cf4a30816dbd431077215ec08298df7eb0e069e
Osnir Teixeira Costa <osnirteixeiracosta@Mac-Osnir.local>
2023-06-25 18:41:31 -0300

    feat: initial project file

---
+++ consumer-basic/src/main/java/br/com/osnirmaster/consumer/app/AppApplication.java
@@ -0,0 +1,19 @@
+package br.com.osnirmaster.consumer.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@SpringBootApplication
+@EnableSpringDataWebSupport
+@EnableCaching
+/* For war file artifact
+ * public class ForumApplication extends SpringBootServletInitializer {
+ */
+public class AppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+
+}
