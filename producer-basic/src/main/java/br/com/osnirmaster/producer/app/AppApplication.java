c88b4da159994c147294f3cd77bd36770dbef153
osnirmaster@hotmail.com <titaniun08>
2023-06-27 22:59:54 -0300

    feat: include collection

--- producer-basic/src/main/java/br/com/osnirmaster/producer/app/AppApplication.java
+++ producer-basic/src/main/java/br/com/osnirmaster/producer/app/AppApplication.java
@@ -2,12 +2,11 @@ package br.com.osnirmaster.producer.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
 
 @SpringBootApplication
-@EnableSpringDataWebSupport
-@EnableCaching
+//@EnableSpringDataWebSupport
+//@EnableCaching
 /* For war file artifact
  * public class ForumApplication extends SpringBootServletInitializer {
  */
